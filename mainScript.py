24bbb22c74e6946806939558d7e352723804da46
Muthonikiboi <kiboijoye254@gmail.com>
2025-03-25 17:43:00 +0300

    ADD:Data Visualization and Saving cleaned Data

--- mainScript.py
+++ mainScript.py
@@ -1,5 +1,8 @@
 #Fuctions for grouping columns together nad determining the number of accients occurring due to different conmditions
 
+# Reference:
+# https://www.geeksforgeeks.org/how-to-reset-index-after-groupby-pandas/
+# https://stackoverflow.com/questions/59699487/how-does-pandas-groupby-and-reset-index-change-the-dataframes-index
 import pandas as pd
 
 def groups(df, columns, rename = "No of Accidents"):
